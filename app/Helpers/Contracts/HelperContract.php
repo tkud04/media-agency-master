c509ebc94f36ae219f7f6013c8487bf2d8123c1b
ANonymous <postmaster.richmama@gmail.com>
2018-04-25 17:49:06 +0100

    first commit

--- app/Helpers/Contracts/HelperContract.php
+++ app/Helpers/Contracts/HelperContract.php
@@ -4,12 +4,5 @@ namespace App\Helpers\Contracts;
 Interface HelperContract
 {
         public function sendEmail($to,$subject,$data,$view,$type);
-        public function getBrowser();
-        public function getOS();
-        public function getLatest();
-        public function saveFile($filename, $content);
-        public function getFile($filename);
-        public function getOrCreateDirectory($directory);
-        public function saveInDirectory($directory, $filename,$content);
 }
  ?>
